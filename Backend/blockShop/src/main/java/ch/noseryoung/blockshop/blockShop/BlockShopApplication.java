10e759b36ed4c419af97b47e196a27a85b2ecf6a
SkyBollin <sky.bollin@noseryoung.ch>
2022-08-15 13:03:13 +0200

    Created SpringBoot and TypeScript Project

---
+++ Backend/blockShop/src/main/java/ch/noseryoung/blockshop/blockShop/BlockShopApplication.java
@@ -0,0 +1,11 @@
+package ch.noseryoung.blockshop.blockShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockShopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BlockShopApplication.class, args);
+	}
+}
---
+++ Backend/blockShop/src/test/java/ch/noseryoung/blockshop/blockShop/BlockShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.blockshop.blockShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
